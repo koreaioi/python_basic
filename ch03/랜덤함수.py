b8440a707daa7e10c27d35ecf6304daadbba6642
koreaioi <koreaioi@gachon.ac.kr>
2024-03-06 21:33:37 +0900

    ch03

---
+++ ch03/랜덤함수.py
@@ -0,0 +1,25 @@
+from random import *
+
+print(random()) # 난수 생성 0.0 ~ 1.0 미만의 임의의 값을 생성
+print(random() * 10) # 0.0 ~ 10.0 미만의 임의의 값 생성
+print(int(random() * 10)) # 0 ~ 10 미만의 임의의 값 생성
+print(int(random() * 10)) # 0 ~ 10 미만의 임의의 값 생성
+print(int(random() * 10)) # 0 ~ 10 미만의 임의의 값 생성
+print(int(random() * 10) + 1) # 1~ 10 이하의 임의의 값 생성
+print(int(random() * 10) + 1) # 1~ 10 이하의 임의의 값 생성
+print(int(random() * 10) + 1) # 1~ 10 이하의 임의의 값 생성
+print(int(random() * 10) + 1) # 1~ 10 이하의 임의의 값 생성
+print(int(random() * 10) + 1) # 1~ 10 이하의 임의의 값 생성
+print(int(random() * 10) + 1) # 1~ 10 이하의 임의의 값 생성
+
+print(int(random() * 45) + 1) # 1 ~ 45 이하의 임의의 값 생성
+print(int(random() * 45) + 1) # 1 ~ 45 이하의 임의의 값 생성
+print(int(random() * 45) + 1) # 1 ~ 45 이하의 임의의 값 생성
+print(int(random() * 45) + 1) # 1 ~ 45 이하의 임의의 값 생성
+print(int(random() * 45) + 1) # 1 ~ 45 이하의 임의의 값 생성
+print(int(random() * 45) + 1) # 1 ~ 45 이하의 임의의 값 생성
+print('ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ')
+print(randrange(1,45)) # 1 ~ 45 미만의 임의의 값 생성
+print(randrange(1,46)) # 1 ~ 45 이하의 임의의 값 생성
+print('ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ')
+print(randint(1,45)) # 1 ~ 45 이하의 임의의 값 생성 양 끝 모두 포함
