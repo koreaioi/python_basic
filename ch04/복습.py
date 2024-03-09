035e495d0b6a481ec4aa29530101081cbcb9008e
koreaioi <koreaioi@gachon.ac.kr>
2024-03-09 23:37:44 +0900

    ch04 - 문자열 복습

---
+++ ch04/복습.py
@@ -0,0 +1,28 @@
+# python = "Python is Amazing"
+#
+# print(python.lower())
+# print(python.upper())
+# print(python[0].isupper())
+# print(python[0].islower())
+#
+# print(len(python))
+# index = python.index("n")
+# print(index)
+#
+# print(python.find("java"))
+# print(python.find("Python")) # 맨 처음 나온 인덱스 기준
+#
+# print(python.count("n"))
+
+# 문자열 포맷 방법
+print("나는 송우석 %d 살 입니다." % 20)
+print("나는 %s를 좋아해요" % "신지민")
+print("Apple은 %c 로 시작해요" % 'A')
+
+print("나는 {}살 입니다.".format(20))
+print("나는 {}살 만나이는 {}살입니다.".format(25, 23))
+print("나는 {1}살 만나이는 {0}살입니다.".format(25, 23))
+
+print("나는 {name}입니다. 나는 {lover}를 좋아해요".format(name ="송우석",lover= "신지민"))
+
+
